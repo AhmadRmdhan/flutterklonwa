a93d29a1dab9570eab53c03618ed00262234c3b6
AhmadRmdhan <ahmadkudus64@gmail.com>
2022-08-09 12:50:44 +0700

    first commit

---
+++ lib/chat_model.dart
@@ -0,0 +1,46 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({required this.name, required this.message, required this.time, required this.avatarUrl});
+}
+
+List<ChatModel> dummyData = [
+  new ChatModel(
+      name: "Vikri",
+      message: "Woi,kamu dimana",
+      time: "16.00",
+      avatarUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9O15cqMYUF1i5apHWKMf9FGaXwCFReY3hsSSE3ak&s"),
+  new ChatModel(
+      name: "Farell",
+      message: "Mabar skuy",
+      time: "16.30",
+      avatarUrl:"https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=740&t=st=1660020081~exp=1660020681~hmac=4e59af2005ccfb06470c2a50c016b6c2087bbd7b5c77fbbd2a8b900db27a6e26"),
+          
+  new ChatModel(
+      name: "Anggi",
+      message: "Ayo -1 ni",
+      time: "16.40",
+      avatarUrl:"https://www.w3schools.com/w3images/avatar2.png"),
+         
+  new ChatModel(
+      name: "Om dud",
+      message: "Gass",
+      time: "16,45",
+      avatarUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOALqcMjby_cOSIr-3GFWRO_zY_j0qcB4zRA&usqp=CAU"),
+          
+  new ChatModel(
+      name: "Sari",
+      message: "Siapa Ini",
+      time: "15.30",
+      avatarUrl:"https://w7.pngwing.com/pngs/193/660/png-transparent-computer-icons-woman-avatar-avatar-girl-thumbnail.png"),
+          
+  new ChatModel(
+      name: "Brian",
+      message: "Kabarmu gmn",
+      time: "13.00",
+      avatarUrl:"https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png"),
+       
+];
